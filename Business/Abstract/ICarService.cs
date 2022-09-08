9f1508ab9a8043cbebbf7ac3e308ef6b9b6c3a6e
sametbakar <sametcb.30@gmail.com>
2022-09-09 01:19:55 +0300

    Updated

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,7 @@ namespace Business.Abstract
         void Add(Car car);
         void Update(Car car);
         void Delete(Car car);
+
+        List<CarDetailDto> GetCarDetails();
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,21 +8,24 @@ using Entities.Concrete;
 
 
 CarManager carManager = new CarManager(new EfCarDal());
-CarAdd(carManager);
 
-GetCarsByBrandId(carManager);
+CarDetailTest(carManager);
 
-GetCrasByColorId(carManager);
+//CarAdd(carManager);
+
+//GetCarsByBrandId(carManager);
+
+//GetCrasByColorId(carManager);
+
+//carManager.Delete(new Car { CarId = 2002 });
+
 
 
 
 static void CarAdd(CarManager carManager)
 {
-    Car car1 = new Car { BrandId = 20, ColorId = 4, ModelYear = 2019, Description = "Hyundai i20" };
-    Car car2 = new Car { BrandId = 1003, ColorId = 1, DailyPrice = 659, ModelYear = 2020, Description = "R" };
-
-   
-
+    Car car1 = new Car { BrandId = 20, ColorId = 4, ModelYear = 2019, DailyPrice=539, Description = "Hyundai i20" };
+    Car car2 = new Car { BrandId = 1003, ColorId = 1, DailyPrice = 659, ModelYear = 2020, Description = "Renault Megan" };
 
     carManager.Add(car1);
     carManager.Add(car2);
@@ -43,5 +46,13 @@ static void GetCrasByColorId(CarManager carManager)
     foreach (var car in carManager.GetCarsByColorId(4))
     {
         Console.WriteLine(car.Description);
+    }
+}
+
+static void CarDetailTest(CarManager carManager)
+{
+    foreach (var car in carManager.GetCarDetails())
+    {
+        Console.WriteLine("{0} / {1} / {2}", car.CarName, car.BrandName, car.ColorName);
     }
 }
